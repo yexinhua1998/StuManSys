dea50e5a655c7bc9a9553a787a657cc019f67de3
Sinward <244649399@qq.com>
2019-01-08 01:35:03 +0800

    修改了主程序文件，加入了用户登录的逻辑

--- StuManSys.py
+++ StuManSys.py
@@ -2,18 +2,44 @@
 
 import web
 from file import files,file_name
+import psycopg2
+import config
 
 urls=(
 	'/','index',
+	'/verification','verification',
 	'/(.+)','file_get'
 )
+
+db_conn=psycopg2.connect(**config.db_conn_config())
 
 class index:
 	def GET(self):
 		return files['login']
 
+class verification:
+	def POST(self):
+		print('verification')
+		input=web.input()
+		cursor=db_conn.cursor()
+		command='SELECT * FROM STUMAN.User WHERE username=\'%s\';'%input.username
+		cursor.execute(command)
+		if cursor.rowcount==0:
+			return files['user_not_exist']
+		else:
+			result=cursor.fetchall()[0]
+			print(result)
+			_,real_pw,role=result
+			real_pw=real_pw.strip()
+			role=role.strip()
+			if real_pw==input.password:
+				return files['verification_success']
+			else:
+				return files['password_incorrect']
+
 class file_get:
 	def GET(self,path):
+		print('fileget')
 		print('path=%s'%(path))
 		return files[file_name[path]]
 
