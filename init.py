04c4d496576ca811bb591f1de42669db276400e0
Sinward <244649399@qq.com>
2019-01-09 00:51:22 +0800

    更改了表的结构，以使得系统可以根据用户名来得到其学生或教师编号增加了视图。增加了视图。

--- init.py
+++ init.py
@@ -10,44 +10,51 @@ from config import db_conn_config
 table_create_command='''
 CREATE SCHEMA STUMAN;
 
+CREATE TABLE STUMAN.User(
+	username char(20) PRIMARY KEY,
+	password char(20),
+	role char(5) CHECK(role IN ('SU','AD','SM','TM','TE','ST')),
+	no char(8) CHECK (no SIMILAR TO '\\d{8}')--符合该正则表达式，即8个数字符号
+);
 CREATE TABLE STUMAN.Teacher(
-	Tno Serial PRIMARY KEY,
+	Tno char(8) PRIMARY KEY,
 	Tname char(10),
 	Tincome float(1),
-	Tloc char(10) CHECK (Tloc IN ('教授','副教授','讲师','研究员'))
+	Tloc char(5) CHECK (Tloc IN ('PR','AP','LE','RE'))
+	--分别为教授、副教授、讲师、研究员
 );
 CREATE TABLE STUMAN.Class(
-	CLAno Serial PRIMARY KEY,
+	CLAno char(8) PRIMARY KEY,
 	CLAmajor char(20),
 	CLAgrade int,
 	CLAid int,
-	CLAtno int,
+	CLAtno char(8),
 	FOREIGN KEY (CLAtno) REFERENCES STUMAN.Teacher(Tno)
 );
 CREATE TABLE STUMAN.Student(
 	Sno char(8) PRIMARY KEY,
 	Sname char(10),
-	CLAno int REFERENCES STUMAN.Class(CLAno),
+	CLAno char(8) REFERENCES STUMAN.Class(CLAno),
 	Sdo char(10)
 );
 CREATE TABLE STUMAN.Course(
-	Cno serial PRIMARY KEY,
+	Cno char(8) PRIMARY KEY,
 	Cname char(30),
 	Ccredit float(1),
 	Cyear int,
 	Cteam char(1) CHECK(Cteam IN ('上','下')),
-	Ctno int REFERENCES STUMAN.Teacher(Tno),
+	Ctno char(8) REFERENCES STUMAN.Teacher(Tno),
 	Ciscom boolean
 );
 CREATE TABLE STUMAN.Score(
 	Sno char(8) REFERENCES STUMAN.Student(Sno),
-	Cno int REFERENCES STUMAN.Course(Cno),
+	Cno char(8) REFERENCES STUMAN.Course(Cno),
 	Score float(1),
 	PRIMARY KEY(Sno,Cno)
 );
 CREATE TABLE STUMAN.CC(
-	CLAno int REFERENCES STUMAN.Class(CLAno),
-	Cno int REFERENCES STUMAN.Course(Cno),
+	CLAno char(8) REFERENCES STUMAN.Class(CLAno),
+	Cno char(8) REFERENCES STUMAN.Course(Cno),
 	PRIMARY KEY (CLAno,Cno)
 );
 '''
@@ -78,6 +85,32 @@ CREATE INDEX cc_cno ON STUMAN.CC USING HASH(Cno);
 
 '''
 
+view_create_command='''
+CREATE VIEW STUMAN.CLA_VIEW AS
+SELECT C.CLAno,C.CLAmajor,C.CLAgrade,C.CLAid,C.CLAtno,T.Tname
+FROM STUMAN.Class C,STUMAN.Teacher T
+WHERE C.CLAtno=T.Tno;
+--班级管理的视图
+
+
+CREATE VIEW STUMAN.COU_VIEW AS
+SELECT C.Cno,C.Cname,C.Ccredit,C.Cyear,C.Cteam,C.Ciscom,C.Ctno,T.Tname
+FROM STUMAN.Course C,STUMAN.Teacher T
+WHERE C.Ctno=T.Tno;
+--课程管理的视图
+
+CREATE VIEW STUMAN.SCO_VIEW AS 
+SELECT 
+	C.Cno,C.Cname,C.Ccredit,C.Cyear,C.Cteam,C.Ciscom,C.Ctno,C.Tname,
+	ST.Sname,ST.Sno,ST.CLAno,
+	CLA.CLAgrade,CLA.CLAmajor,CLA.CLAid
+	,SC.Score
+FROM STUMAN.COU_VIEW C,STUMAN.Student ST,STUMAN.Score SC,STUMAN.Class CLA
+WHERE C.Cno=SC.Cno AND ST.Sno=SC.Sno;
+--成绩管理的视图
+
+'''
+
 if __name__=='__main__':
 	print('connecting database...')
 	conn=psycopg2.connect(**db_conn_config())
@@ -91,6 +124,10 @@ if __name__=='__main__':
 
 	print('creating index...')
 	cursor.execute(index_create_command)
+	print('done')
+
+	print('creating view...')
+	cursor.execute(view_create_command)
 	print('done')
 
 	conn.commit()
