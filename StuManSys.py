41d7b285e42528f11e2a176aaf11277080694c39
Sinward <244649399@qq.com>
2019-01-09 20:55:17 +0800

    完成教师管理和课程管理功能。

--- StuManSys.py
+++ StuManSys.py
@@ -15,7 +15,17 @@ urls=(
 	'/AD','administrator',
 	'/SM','student_management',
 	'/TM','teaching_management',
+
 	'/ad_teacher_management','ad_teacher_management',
+	'/ad_add_teacher','ad_add_teacher',
+	'/ad_delete_teacher','ad_delete_teacher',
+	'/ad_update_teacher','ad_update_teacher',
+
+	'/tm_course_management','tm_course_management',
+	'/tm_add_course','tm_add_course',
+	'/tm_delete_course','tm_delete_course',
+	'/tm_update_course','tm_update_course',
+
 	'/st_score_query','st_score_query',
 	'/logout','logout',
 	'/abandoned','abandoned',
@@ -70,9 +80,6 @@ class verification:
 			else:
 				return files['password_incorrect']
 
-class main:
-	def GET(self):
-		return render.main(session.role,role_showed[session.role])
 
 def role_check(input_role,target_role_list):
 	if input_role in target_role_list:
@@ -120,11 +127,101 @@ class logout:
 
 ##########################下面为功能部分#############################
 
+
+######行政部分######
 class ad_teacher_management:
 	def GET(self):
 		role_check(session.role,['AD'])
 		data=db.query('SELECT * FROM STUMAN.Teacher').list()
 		return render.ad_teacher_management(session.username,data)
+
+class ad_add_teacher:
+	#增加老师的部分
+	def POST(self):
+		role_check(session.role,['AD'])
+		data=web.input()
+		command="INSERT INTO STUMAN.Teacher VALUES('%s','%s',%s,'%s')"\
+		%(data.tno,data.tname,data.tincome,data.tloc)
+		try:
+			db.query(command)
+			return render.operation_result('ad_teacher_management',None)
+		except Exception as e:
+			return render.ad_add_teacher('ad_teacher_management',e)
+
+class ad_delete_teacher:
+	#删除老师
+	def POST(self):
+		role_check(session.role,['AD'])
+		data=web.input()
+		command="DELETE FROM STUMAN.Teacher WHERE Tno='%s'"%data.tno
+		try:
+			db.query(command)
+			return render.operation_result('ad_teacher_management',None)
+		except Exception as e:
+			return render.operation_result('ad_teacher_management',e)
+
+class ad_update_teacher:
+	#修改老师
+	def POST(self):
+		role_check(session.role,['AD'])
+		data=web.input()
+		command="UPDATE STUMAN.Teacher SET Tname='%s',Tincome=%s,Tloc='%s' WHERE Tno='%s'"\
+		%(data.tname,data.tincome,data.tloc,data.tno)
+		try:
+			db.query(command)
+			return render.operation_result('ad_teacher_management',None)
+		except Exception as e:
+			return render.operation_result('ad_teacher_management',e)
+
+######教务处部分######
+class tm_course_management:
+	def GET(self):
+		role_check(session.role,['TM'])
+		command="SELECT * FROM STUMAN.COU_VIEW;"
+		data=db.query(command).list()
+		return render.tm_course_management(session.username,data)
+
+
+class tm_add_course:
+	def POST(self):
+		role_check(session.role,['TM'])
+		data=web.input(ciscom=[])
+		ciscom='True' if len(data.ciscom)==1 else 'False'
+		command="INSERT INTO STUMAN.Course VALUES('%s','%s',%s,%s,'%s','%s',%s)"\
+		%(data.cno,data.cname,data.ccredit,data.cyear,data.cteam,data.ctno,ciscom)
+		try:
+			db.query(command)
+			return render.operation_result('tm_course_management',None)
+		except Exception as e:
+			return render.operation_result('tm_course_management',e)
+
+
+class tm_delete_course:
+	#删除课程的类
+	def POST(self):
+		role_check(session.role,['TM'])
+		data=web.input()
+		command="DELETE FROM STUMAN.Course WHERE cno='%s'"%data.cno 
+		try:
+			db.query(command)
+			return render.operation_result('/tm_course_management',None)
+		except Exception as e:
+			return render.operation_result('/tm_course_management',e)
+
+class tm_update_course:
+	#更新课程的类
+	def POST(self):
+		role_check(session.role,['TM'])
+		data=web.input(ciscom=[])
+		ciscom='True' if len(data.ciscom)==1 else 'False'
+		command="UPDATE STUMAN.Course SET \
+		Cname='%s',Ccredit=%s,Cyear=%s,Cteam='%s',Ctno='%s',Ciscom=%s WHERE Cno='%s'"\
+		%(data.cname,data.ccredit,data.cyear,data.cteam,data.ctno,ciscom,data.cno)
+		try:
+			db.query(command)
+			return render.operation_result('tm_course_management',None)
+		except Exception as e:
+			return render.operation_result('tm_course_management',e)
 
 class st_score_query:
 	'''
